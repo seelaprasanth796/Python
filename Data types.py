6848d257988441ab0f07125ced78857cff34ca46
Prasanth <seelaprasanth796@gmail.com>
2026-07-09 14:04:04 +0530

    Add files via upload

---
+++ Data types.py
@@ -0,0 +1,60 @@
+Python 3.14.4 (v3.14.4:23116f998f6, Apr  7 2026, 09:45:22) [Clang 17.0.0 (clang-1700.6.4.2)] on darwin
+Enter "help" below or click "Help" above for more information.
+>>> #data types
+>>> a=5
+>>> type(a)
+<class 'int'>
+>>> b=7.8
+>>> type(b)
+<class 'float'>
+>>> c='code'
+>>> type(c)
+<class 'str'>
+>>> c="code"
+>>> type(c
+...      0
+...      
+SyntaxError: '(' was never closed
+>>> c="code"
+...      
+>>> type(c)
+...      
+<class 'str'>
+>>> e='''python'''
+...      
+>>> type(e)
+...      
+<class 'str'>
+>>> f"c"
+...      
+'c'
+>>> f="c"
+...      
+>>> type(f)
+...      
+<class 'str'>
+>>> x=5+9j
+...      
+>>> type(x)
+...      
+<class 'complex'>
+>>> y=6j+9
+...      
+>>> type(y)
+...      
+<class 'complex'>
+>>> z=6j
+...      
+>>> type(z)
+...      
+<class 'complex'>
+>>> a=(True)
+...      
+>>> type(a)
+...      
+<class 'bool'>
+>>> b=(False)
+...      
+>>> type(b)
+...      
+<class 'bool'>
