21d42dabb6aca112290f3e4d9b60430158e0b3e7
Prasanth <seelaprasanth796@gmail.com>
2026-07-10 15:58:37 +0530

    Add files via upload

---
+++ striding.py
@@ -0,0 +1,20 @@
+Python 3.14.4 (v3.14.4:23116f998f6, Apr  7 2026, 09:45:22) [Clang 17.0.0 (clang-1700.6.4.2)] on darwin
+Enter "help" below or click "Help" above for more information.
+>>> #striding
+>>> a="data science"
+>>> a[::]
+'data science'
+>>> a[::1]
+'data science'
+>>> a[::2]
+'dt cec'
+>>> 
+>>> 
+>>> #task 1
+>>> a="machine learning"
+>>> a[::4]
+'miln'
+>>> a[::7]
+'m n'
+>>> a[::2]
+'mcielann'
