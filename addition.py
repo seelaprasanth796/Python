ca8fd2ce3317f4b341337cbb936587df90d9ff64
Prasanth <seelaprasanth796@gmail.com>
2026-07-14 15:56:55 +0530

    Add files via upload

---
+++ addition.py
@@ -0,0 +1,46 @@
+'''a=2
+b=4
+print(a+b)'''
+
+
+'''a=3
+b=9
+c=a+b
+print(c)'''
+
+#run_time input()
+'''a=int(input("enter the a value"))
+b=int(input("enter the b value"))
+print(a+b)'''
+
+'''a=float(input("enter the a value"))
+b=float(input("enter the b value"))
+print(a+b)'''
+
+#str
+'''a=str(input("data1"))
+b=str(input("data2"))
+print(a+b)'''
+
+'''fname=input("first name")
+lname=input("last name")
+print((fname+" "+lname).title())'''
+
+'''city=input("vjw")
+print(city)'''
+
+
+'''a=complex(input("enter the a value"))
+b=complex(input("enter the b value"))
+print(a+b)'''
+
+'''a=bool(input("enter the a value"))
+b=bool(input("enter the b value"))
+print(a+b)'''
+
+'''a=int(input())
+b=int(input())
+print(a+b)'''
+
+a=input()
+print(a)
